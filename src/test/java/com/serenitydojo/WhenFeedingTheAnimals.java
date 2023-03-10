edb8c3f973d2b8e2da5e0c2acfe299e38e33c707
VJExplorer <varuni_j@yahoo.com>
2023-03-10 12:51:45 +0000

    Exercise 7 Completed Changes to WhenFeedingTheAnimals classs

--- src/test/java/com/serenitydojo/WhenFeedingTheAnimals.java
+++ src/test/java/com/serenitydojo/WhenFeedingTheAnimals.java
@@ -13,36 +13,36 @@ public class WhenFeedingTheAnimals {
     public void shouldFeedCatsTuna() {
         Feeder feeder = new Feeder();
 
-        String food = feeder.feeds("Cat", false).name();
+        FoodType food = feeder.feeds("Cat", false);
 
-        Assert.assertEquals(TUNA.name(), food);
+        Assert.assertEquals(TUNA, food);
     }
 
     @Test
     public void shouldFeedHamstersCabbage() {
         Feeder feeder = new Feeder();
 
-        String food = feeder.feeds("Hamster", false).name();
+        FoodType food = feeder.feeds("Hamster", false);
 
-        Assert.assertEquals(CABBAGE.name(), food);
+        Assert.assertEquals(CABBAGE, food);
     }
 
     @Test
     public void shouldFeedDogsDogFood() {
         Feeder feeder = new Feeder();
 
-        String food = feeder.feeds("Dog", false).name();
+        FoodType food = feeder.feeds("Dog", false);
 
-        Assert.assertEquals(DOG_FOOD.name(), food);
+        Assert.assertEquals(DOG_FOOD, food);
     }
 
     @Test
     public void shouldFeedPremiumCatsPremiumFood() {
         Feeder feeder = new Feeder();
 
-        String food = feeder.feeds("Cat", true).name();
+        FoodType food = feeder.feeds("Cat", true);
 
-        Assert.assertEquals(SALMON.name(), food);
+        Assert.assertEquals(SALMON, food);
 
     }
 }
